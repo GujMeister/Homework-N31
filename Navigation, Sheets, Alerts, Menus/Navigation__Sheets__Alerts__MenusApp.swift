fcff49bcb00fc8f84ba702009991777315880687
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-05-28 23:20:26 +0400

    Initial Commit

---
+++ Navigation, Sheets, Alerts, Menus/Navigation__Sheets__Alerts__MenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation__Sheets__Alerts__MenusApp.swift
+//  Navigation, Sheets, Alerts, Menus
+//
+//  Created by Luka Gujejiani on 28.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Navigation__Sheets__Alerts__MenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
